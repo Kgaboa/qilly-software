dbc226746efee58b4ec33fcc5631dc5b91d253ed
Kgaboa <101918519+Kgaboa@users.noreply.github.com>
2026-09-21 12:25:04 +0200

    Remove BuildAid reference banner

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,7 +11,6 @@ import { PublicPartnerPortal } from './components/PublicPartnerPortal';
 import { PartnerLogin } from './components/PartnerLogin';
 import { PartnerPortal } from './components/PartnerPortal';
 import { Footer } from './components/Footer';
-import { BuildAidReferenceBar } from './components/BuildAidReferenceBar';
 import { CapitalRaisingGuide } from './components/CapitalRaisingGuide';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
@@ -91,7 +90,6 @@ export default function App() {
   return (
     <>
       <Toaster position="top-right" richColors />
-      <BuildAidReferenceBar />
       {currentView === 'auth' && (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100">
           <AuthForm
